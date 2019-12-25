beb0c87609af3a156b7d8a2d61d6a0677e79aace
Weixuan Zhang <39925558+WeixuanZ@users.noreply.github.com>
2019-12-25 09:54:09 +0800

    Added Postprocessor.py: signal processing using median filter and convolution with FFT

---
+++ src/Postprocessor.py
@@ -0,0 +1,20 @@
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt
+
+# signal processing techniques to remove outlier and smooth the plot
+
+def smooth(vec):
+    vec = signal.medfilt(vec, kernel_size=51)
+    win = signal.hann(51)
+    vec = signal.fftconvolve(vec, win, mode='same') / sum(win)
+
+    return vec
+
+
+if __name__ == '__main__':
+    vec = np.loadtxt('../data/data.csv', delimiter=',')
+    plt.plot(np.arange(1,len(vec)+1,1),vec)
+    plt.plot(np.arange(1,len(vec)+1,1),smooth(vec))
+    plt.ylim([7.5e6, 7.6e6])
+    plt.show()
