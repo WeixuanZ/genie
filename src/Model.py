1ca17cbb3ee489493bea1dfffce1a32953c208c8
Weixuan Zhang <39925558+WeixuanZ@users.noreply.github.com>
2019-12-25 22:07:12 +0800

    Updated training file

--- src/Model.py
+++ src/Model.py
@@ -4,12 +4,12 @@ os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 # os.environ["KERAS_BACKEND"] = "tensorflow"
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.optimizers import SGD
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.optimizers import SGD, Adam
 # from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
 from scipy.io import loadmat
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
 from Preprocessor import *
 
@@ -25,6 +25,7 @@ def build_model(input_shape=(32, 32, 3)):
     # VGG-like convnet
     model = Sequential()
     model.add(Conv2D(num_filters, kernel_size, input_shape=input_shape, activation='relu'))
+    # model.add(BatchNormalization())
     model.add(Conv2D(num_filters, kernel_size, activation='relu'))
     model.add(MaxPooling2D(pool_size=pool_size))
     # (16, 8, 32)
@@ -34,14 +35,15 @@ def build_model(input_shape=(32, 32, 3)):
     model.add(Conv2D(num_filters * 2, kernel_size, activation='relu'))
     model.add(MaxPooling2D(pool_size=pool_size))
     # (8, 4, 64) = (2048)
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Flatten())
     model.add(Dense(1024, activation='relu'))
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(11, activation='softmax'))
 
+    # model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.1, decay=0.01), metrics=['accuracy'])
     model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
 
     return model
@@ -59,18 +61,23 @@ def train_model(model,batch_size=128, epoch=5, save_file='../model/detector_mode
     x_extra, y_extra = load_data('../model/extra.mat')
 
     # x_train, y_train = np.concatenate([x_train, x_extra])[:-1000], np.concatenate([y_train, y_extra])[:-1000]
-    # x_val, y_val = np.concatenate([x_train, x_extra])[-1000:], np.concatenate([y_train, y_extra])[-1000:]
+    x_val, y_val = x_extra[-2000:], y_extra[-2000:]
 
-    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epoch, verbose=1, )
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epoch, verbose=1, validation_data=(x_val, y_val))
     score = model.evaluate(x_test, y_test, verbose=1)
     print('Test score:', score[0])
     print('Test accuracy:', score[1])
     model.save(save_file)
-    pyplot.plot(history.history['loss'])
-    pyplot.show()
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model Loss')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend(['train', 'validate'], loc='upper left')
+    plt.show()
 
 
-train_model(build_model(), epoch=10, save_file='../model/detector_model_3.hdf5')
+train_model(build_model(), epoch=15, batch_size=128, save_file='../model/detector_model_2.hdf5')
 
 
 
